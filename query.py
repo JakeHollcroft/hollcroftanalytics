ca766c5562b19425b9dc0ae0b45524a70aff3ec9
Jake Hollcroft <jakehollcroft918@gmail.com>
2026-01-07 04:04:32 -0600

    updated query

--- query.py
+++ query.py
@@ -1,6 +1,11 @@
 import duckdb
 from pathlib import Path
 import os
+import pandas as pd
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
 
 DB_FILE = Path(os.environ.get("PERSIST_DIR", ".")) / "housecall_data.duckdb"
 
